744dc50706191b2a1b543defface411636c87c82
Arseny <me@evindor.com>
2017-04-07 00:45:32 +0300

    Initial Commit

---
+++ Keystroke/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Keystroke
+//
+//  Created by Arseny Zarechnev on 07/04/2017.
+//  Copyright © 2017 Sabaka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
