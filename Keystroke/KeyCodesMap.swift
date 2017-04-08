764cf5e77204542e108fa5f18427b69b4fcfaed0
Arseny <me@evindor.com>
2017-04-08 21:02:18 +0300

    KeyMaps

--- Keystroke/KeyCodesMap.swift
+++ Keystroke/KeyCodesMap.swift
@@ -7,7 +7,80 @@
 //
 
 enum KeyCodesMap: UInt64 {
+    // Row 1
+    case Key_BACKTICK = 50
     case Key_1 = 18
     case Key_2 = 19
     case Key_3 = 20
+    case Key_4 = 21
+    case Key_5 = 23
+    case Key_6 = 22
+    case Key_7 = 26
+    case Key_8 = 28
+    case Key_9 = 25
+    case Key_0 = 29
+    case Key_DASH = 27
+    case Key_EQUALS = 24
+    case Key_DELETE = 51
+
+    // Row 2
+    case Key_TAB = 48
+    case Key_q = 12
+    case Key_w = 13
+    case Key_e = 14
+    case Key_r = 15
+    case Key_t = 17
+    case Key_y = 16
+    case Key_u = 32
+    case Key_i = 34
+    case Key_o = 31
+    case Key_p = 35
+    case Key_OPEN_SQUARE_BRACKET = 33
+    case Key_CLOSE_SQUARE_BRACKET = 30
+
+    // Row 3
+    case Key_a = 0
+    case Key_s = 1
+    case Key_d = 2
+    case Key_f = 3
+    case Key_g = 5
+    case Key_h = 4
+    case Key_j = 38
+    case Key_k = 40
+    case Key_l = 37
+    case Key_SEMICOLON = 41
+    case Key_QUOTES = 39
+    case Key_RETURN = 36
+
+    // Row 4
+    case Key_z = 6
+    case Key_x = 7
+    case Key_c = 8
+    case Key_v = 9
+    case Key_b = 11
+    case Key_n = 45
+    case Key_m = 46
+    case Key_COMMA = 43
+    case Key_DOT = 47
+    case Key_SLASH = 44
+    
+    // Row 5
+    case Key_SPACE = 49
+    case Key_ARROW_LEFT = 123
+    case Key_ARROW_RIGHT = 124
+    case Key_ARROW_DOWN = 125
+    case Key_ARROW_UP = 126
+    
+    case Key_COMMAND_LEFT = 55
+    case Key_COMMAND_RIGHT = 54
+    
+    case Key_SHIFT_LEFT = 56
+    case Key_SHIFT_RIGHT = 60
+    
+    case Key_CONTROL = 59
+    
+    case Key_OPTION_LEFT = 58
+    case Key_OPTION_RIGHT = 61
+    
+    case Key_FN = 63
 }
