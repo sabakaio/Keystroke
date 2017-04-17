9f28001261daec6541db39ed38b17b86ecbeb155
Arseny <me@evindor.com>
2017-04-17 21:17:40 +0300

    implement menu bar icon

--- Keystroke/AppDelegate.swift
+++ Keystroke/AppDelegate.swift
@@ -21,10 +21,23 @@ let eventLoopListener = EventLoopListener()
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // First load configurations from files
         appConfigManager.loadConfigurationsFromBundle()
         eventLoopListener.start()
+        
+        // status menu
+        let icon = NSImage(named: "StatusIcon")
+//        icon!.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
